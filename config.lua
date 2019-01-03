a1fc16162d06d8300d49b4f0d90bf0e2adc2f7f4
Hellfar <pierre.louchet@gmail.com>
2019-01-03 15:07:58 +0100

    Windows and Android good configuration.

--- config.lua
+++ config.lua
@@ -5,25 +5,56 @@ REPOSITORY_URL = "https://github.com/foundpatterns/packages"
 MANIFEST_FILE = "manifest.scl"
 
 if torchbear.os == "linux" then
-  REPOSITORY_HOME = os.getenv("HOME") .. "/.mp/packages/"
-  -- requires sudo
-  MP_HOME = "/usr/share/mp/"
-  INSTALL_HOME = "/usr/share/"
-  -- INSTALL_HOME = os.getenv("HOME") .. "/.mp/"
-  BIN_HOME = "/usr/bin/"
+  HOME = os.getenv("HOME")
+  -- where repo exists
+  REPOSITORY_HOME = HOME .. "/.mp/packages/"
 
+  --  location of machu-picchu
+  MP_HOME = "/usr/local/machu-picchu/"
+
+  -- where the packages installed from repositories go
+  INSTALL_HOME = "/usr/local/"
+
+  -- where the symlink to the installed packages(from repo) exist
+  BIN_HOME = "/usr/local/bin/"
+
+  -- for dependency management
   DEFAULT_PATH           = "./"
   DEFAULT_SAVE_DIRECTORY = ".mp/"
 elseif torchbear.os == "windows" then
   -- FIXME windows support
   -- TODO alternative for windows
 
-  REPOSITORY_HOME = os.getenv("HOME") .. "/.mp/packages/"
-  MP_HOME = "/usr/share/mp/"
-  INSTALL_HOME = "/usr/share/"
-  -- INSTALL_HOME = os.getenv("HOME") .. "/.mp/"
-  BIN_HOME = "/usr/bin/"
+  -- where repo exists
+  REPOSITORY_HOME = "%AppData%/Roaming/.mp/packages/"
 
+  --  location of machu-picchu
+  MP_HOME = "%ProgramFiles%/mp/"
+
+  -- where the packages installed from repositories go
+  INSTALL_HOME = MP_HOME
+
+  -- where the symlink to the installed packages(from repo) exist
+  BIN_HOME = os.getenv("CMDER_ROOT") .. "/bin"
+
+  -- for dependency management
+  DEFAULT_PATH           = "./"
+  DEFAULT_SAVE_DIRECTORY = ".mp/"
+elseif torchbear.os == "android"
+  HOME = os.getenv("HOME")
+  -- where repo exists
+  REPOSITORY_HOME = HOME .. "/.mp/packages/"
+
+  --  location of machu-picchu
+  MP_HOME = "/usr/local/machu-picchu/"
+
+  -- where the packages installed from repositories go
+  INSTALL_HOME = "/usr/local/"
+
+  -- where the symlink to the installed packages(from repo) exist
+  BIN_HOME = "/data/data/com.termux/files/usr/bin"
+
+  -- for dependency management
   DEFAULT_PATH           = "./"
   DEFAULT_SAVE_DIRECTORY = ".mp/"
 end
